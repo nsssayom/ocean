82fbf6271b51ca6219f7841248b5e98fb4b48176
nsssayom <nsssayom@gmail.com>
2022-10-15 20:01:10 +0600

    lat/lon conversion done

--- main.py
+++ main.py
@@ -15,15 +15,48 @@ dtime = netCDF4.num2date(time_var[:], time_var.units)
 
 sst = nc.variables["sea_surface_temperature"]
 
-print(sst)
+# print(sst)
 
-print(lat)
-print(lon)
-print(dtime[0])
+# print(lat)
+# print(lon)
+# print(dtime[0])
 
-print(sst[0, 2000, 2000])
+print(lat[0])
+print(lat[4320 - 1])
 
-# loop from 0 to 4320
-for i in range(2000, 4320):
-    for j in range(2000, 8640):
-        print(lat[i], lon[j], sst[0, i, j])
+print(lon[0])
+print(lon[8640 - 1])
+
+
+# print(sst[0, 2000, 2000])
+
+# # loop from 0 to 4320
+# for i in range(2000, 4320):
+#     for j in range(2000, 8640):
+#         print(lat[i], lon[j], sst[0, i, j])
+
+
+def getLatIndex(lat):
+    max_index = 4320
+    min_index = 0
+    max_lat = -90
+    min_lat = 90
+
+    factor = max_index / (max_lat - min_lat)
+
+    return int((max_index / 2) + factor * lat)
+
+
+def getLonIndex(lon):
+    max_index = 8640
+    min_index = 0
+    max_lon = 180
+    min_lon = -180
+
+    factor = max_index / (max_lon - min_lon)
+
+    return int((max_index / 2) + factor * lon)
+
+
+print(getLonIndex(85))
+print(lon[getLonIndex(85)])
